48c74fbe5e343858bf037292358f55395c9daf69
Shawn OY <shawn.oy@z-tron.net>
2018-08-13 18:56:46 +0800

    Change config from JSON to JavaScript

--- config/config.js
+++ config/config.js
@@ -4,7 +4,11 @@ module.exports = {
     storage: './database.sqlite3'
   },
   test: {
-    dialect: 'sqlite'
+    dialect: 'postgres',
+    username: 'postgres',
+    password: null,
+    database: 'database_production',
+    host: '127.0.0.1'
   },
   'production': {
     dialect: 'postgres',
